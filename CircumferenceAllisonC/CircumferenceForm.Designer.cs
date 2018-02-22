cdf42d833ddfa50f0fa44b6baee7233f4c0bfed1
AllisonCook27 <allison.cook@ocsbstudent.ca>
2018-02-22 13:03:13 -0500

    adding

--- CircumferenceAllisonC/CircumferenceForm.Designer.cs
+++ CircumferenceAllisonC/CircumferenceForm.Designer.cs
@@ -28,20 +28,117 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblCircumference = new System.Windows.Forms.Label();
+            this.lblAnswerText = new System.Windows.Forms.Label();
+            this.lblEnterRadius = new System.Windows.Forms.Label();
+            this.mnuMenu = new System.Windows.Forms.MenuStrip();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.mniFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.mnuMenu.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // lblCircumference
+            // 
+            this.lblCircumference.AutoSize = true;
+            this.lblCircumference.Font = new System.Drawing.Font("GOST Common", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCircumference.Location = new System.Drawing.Point(295, 182);
+            this.lblCircumference.Name = "lblCircumference";
+            this.lblCircumference.Size = new System.Drawing.Size(0, 19);
+            this.lblCircumference.TabIndex = 0;
+            // 
+            // lblAnswerText
+            // 
+            this.lblAnswerText.AutoSize = true;
+            this.lblAnswerText.Font = new System.Drawing.Font("GOST Common", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswerText.Location = new System.Drawing.Point(31, 182);
+            this.lblAnswerText.Name = "lblAnswerText";
+            this.lblAnswerText.Size = new System.Drawing.Size(135, 19);
+            this.lblAnswerText.TabIndex = 1;
+            this.lblAnswerText.Text = "The circumferen is:";
+            // 
+            // lblEnterRadius
+            // 
+            this.lblEnterRadius.AutoSize = true;
+            this.lblEnterRadius.Font = new System.Drawing.Font("GOST Common", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnterRadius.Location = new System.Drawing.Point(12, 67);
+            this.lblEnterRadius.Name = "lblEnterRadius";
+            this.lblEnterRadius.Size = new System.Drawing.Size(262, 19);
+            this.lblEnterRadius.TabIndex = 2;
+            this.lblEnterRadius.Text = "Enter the radius for the circle (in cm):";
+            // 
+            // mnuMenu
+            // 
+            this.mnuMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniFile});
+            this.mnuMenu.Location = new System.Drawing.Point(0, 0);
+            this.mnuMenu.Name = "mnuMenu";
+            this.mnuMenu.Size = new System.Drawing.Size(439, 24);
+            this.mnuMenu.TabIndex = 3;
+            this.mnuMenu.Text = "menuStrip1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(299, 69);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // mniFile
+            // 
+            this.mniFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniExit});
+            this.mniFile.Name = "mniFile";
+            this.mniFile.Size = new System.Drawing.Size(37, 20);
+            this.mniFile.Text = "File";
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(152, 22);
+            this.mniExit.Text = "Exit";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(179, 136);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 5;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
             // 
             // frmCircumference
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(439, 261);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblEnterRadius);
+            this.Controls.Add(this.lblAnswerText);
+            this.Controls.Add(this.lblCircumference);
+            this.Controls.Add(this.mnuMenu);
+            this.MainMenuStrip = this.mnuMenu;
             this.Name = "frmCircumference";
-            this.Text = "Form1";
+            this.Text = "Cirumference of a Circle by Allison C";
+            this.mnuMenu.ResumeLayout(false);
+            this.mnuMenu.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblCircumference;
+        private System.Windows.Forms.Label lblAnswerText;
+        private System.Windows.Forms.Label lblEnterRadius;
+        private System.Windows.Forms.MenuStrip mnuMenu;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolStripMenuItem mniFile;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
+        private System.Windows.Forms.Button btnCalculate;
     }
 }
 
--- CircumferenceAllisonC/CircumferenceForm.cs
+++ CircumferenceAllisonC/CircumferenceForm.cs
@@ -16,5 +16,6 @@ namespace CircumferenceAllisonC
         {
             InitializeComponent();
         }
+
     }
 }
