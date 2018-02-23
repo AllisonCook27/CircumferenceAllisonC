cc799e98f0bb5cefee152b243852a809dbbeb124
Allison <36515870+AllisonCook27@users.noreply.github.com>
2018-02-23 16:02:27 -0500

    Add files via upload

---
+++ CircumferenceAllisonC/CircumferenceAllisonC-master/CircumferenceAllisonC/CircumferenceForm.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace CircumferenceAllisonC
+{
+    partial class frmCircumference
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCircumference = new System.Windows.Forms.Label();
+            this.lblAnswerText = new System.Windows.Forms.Label();
+            this.lblEnterRadius = new System.Windows.Forms.Label();
+            this.mnuMenu = new System.Windows.Forms.MenuStrip();
+            this.mniFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtRadius = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.mnuMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblCircumference
+            // 
+            this.lblCircumference.AutoSize = true;
+            this.lblCircumference.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCircumference.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblCircumference.Location = new System.Drawing.Point(295, 197);
+            this.lblCircumference.Name = "lblCircumference";
+            this.lblCircumference.Size = new System.Drawing.Size(0, 20);
+            this.lblCircumference.TabIndex = 0;
+            // 
+            // lblAnswerText
+            // 
+            this.lblAnswerText.AutoSize = true;
+            this.lblAnswerText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswerText.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblAnswerText.Location = new System.Drawing.Point(33, 197);
+            this.lblAnswerText.Name = "lblAnswerText";
+            this.lblAnswerText.Size = new System.Drawing.Size(159, 20);
+            this.lblAnswerText.TabIndex = 1;
+            this.lblAnswerText.Text = "The circumference is:";
+            // 
+            // lblEnterRadius
+            // 
+            this.lblEnterRadius.AutoSize = true;
+            this.lblEnterRadius.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnterRadius.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblEnterRadius.Location = new System.Drawing.Point(12, 67);
+            this.lblEnterRadius.Name = "lblEnterRadius";
+            this.lblEnterRadius.Size = new System.Drawing.Size(267, 20);
+            this.lblEnterRadius.TabIndex = 2;
+            this.lblEnterRadius.Text = "Enter the radius for the circle (in cm):";
+            // 
+            // mnuMenu
+            // 
+            this.mnuMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniFile});
+            this.mnuMenu.Location = new System.Drawing.Point(0, 0);
+            this.mnuMenu.Name = "mnuMenu";
+            this.mnuMenu.Size = new System.Drawing.Size(439, 24);
+            this.mnuMenu.TabIndex = 3;
+            this.mnuMenu.Text = "menuStrip1";
+            // 
+            // mniFile
+            // 
+            this.mniFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniExit});
+            this.mniFile.Name = "mniFile";
+            this.mniFile.Size = new System.Drawing.Size(37, 20);
+            this.mniFile.Text = "File";
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(152, 22);
+            this.mniExit.Text = "Exit";
+            this.mniExit.Click += new System.EventHandler(this.mniExit_Click);
+            // 
+            // txtRadius
+            // 
+            this.txtRadius.Location = new System.Drawing.Point(299, 69);
+            this.txtRadius.Name = "txtRadius";
+            this.txtRadius.Size = new System.Drawing.Size(100, 20);
+            this.txtRadius.TabIndex = 4;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.BackColor = System.Drawing.Color.White;
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(175, 140);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(83, 29);
+            this.btnCalculate.TabIndex = 5;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = false;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // frmCircumference
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.ClientSize = new System.Drawing.Size(439, 261);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtRadius);
+            this.Controls.Add(this.lblEnterRadius);
+            this.Controls.Add(this.lblAnswerText);
+            this.Controls.Add(this.lblCircumference);
+            this.Controls.Add(this.mnuMenu);
+            this.MainMenuStrip = this.mnuMenu;
+            this.Name = "frmCircumference";
+            this.Text = "Cirumference of a Circle by Allison C";
+            this.mnuMenu.ResumeLayout(false);
+            this.mnuMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCircumference;
+        private System.Windows.Forms.Label lblAnswerText;
+        private System.Windows.Forms.Label lblEnterRadius;
+        private System.Windows.Forms.MenuStrip mnuMenu;
+        private System.Windows.Forms.TextBox txtRadius;
+        private System.Windows.Forms.ToolStripMenuItem mniFile;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ CircumferenceAllisonC/CircumferenceAllisonC-master/CircumferenceAllisonC/CircumferenceForm.cs
@@ -0,0 +1,71 @@
+﻿/*
+ * Created by: Allison Cook
+ * Created on: 22 February, 2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #10 - circumference
+ * This program calculates the circumference of a circle for a given radius
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CircumferenceAllisonC
+{
+    public partial class frmCircumference : Form
+    {
+        //Local variables
+        double radius, circumference;
+        //local constant
+        const double PI = 3.14;
+
+        public frmCircumference()
+        {
+            InitializeComponent();
+            lblCircumference.Hide();
+            lblAnswerText.Hide();
+        }
+
+        private void mniExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                radius = double.Parse(txtRadius.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Please input a number.");
+            }
+
+            if (radius >= 0)
+            {
+                //calculating the circumference
+                circumference = radius * PI * 2;
+                //rounding to two decimal places
+                circumference = Math.Round(circumference, 2);
+                //setting the answer to text
+                lblCircumference.Text = Convert.ToString(circumference) + "cm" ;
+                //show answer
+                lblAnswerText.Show();
+                lblCircumference.Show();
+
+            }
+            else
+            {
+                MessageBox.Show("Please input a positive number.");
+            }
+
+
+        }
+    }
+}
